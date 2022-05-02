7e0e2a31114f93fcdd2ef08d8690b2c7586ce4e7
gustavo@mumuki.org <gustavo@mumuki.org>
2022-05-02 17:53:07 +0000

    Mumuki Export on 2022-05-02 17:53:07 +0000

--- 00002_¿Y el resto de las ganancias_/extra.js
+++
@@ -1,7 +0,0 @@
-function gananciaTotal(balancesDePeriodo) {
-  let sumatoria = 0;
-  for (let balance of balancesDePeriodo) {
-    sumatoria = sumatoria + balance.ganancia;
-  }
-  return sumatoria;
-}

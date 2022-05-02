701707617c34cd0b9b8d7a7add8529c26c15166e
gustavo@mumuki.org <gustavo@mumuki.org>
2022-05-02 18:57:19 +0000

    Mumuki Export on 2022-05-02 18:57:19 +0000

--- 00006_La ganancia promedio/extra.js
+++
@@ -1,8 +0,0 @@
-function gananciaTotal(balancesDeUnPeriodo) {
-  let sumatoria = 0;
-  for (let balance of balancesDeUnPeriodo) {
-    sumatoria = sumatoria + balance.ganancia;
-  }
-  return sumatoria;
-}
-
--- 00014_Esto es lo máximo/extra.js
+++
@@ -1,9 +0,0 @@
-/*...solution[8]...*/
-
-function maximo(numeros) /*<elipsis-for-student@*/ {
-  return Math.max.apply(null, numeros)
-} /*@elipsis-for-student>*/
-// Retorná el número más grande de una lista de números:
-//
-//  ム maximo([4, 8, 87, 3])
-//  87
